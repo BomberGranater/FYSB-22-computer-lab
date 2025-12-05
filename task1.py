0ad46d3e2f6c205784a51507cc88ce449d4adb14
Lowe Berg <loweberg04@gmail.com>
2025-12-05 14:09:27 +0100

    started task 1 and edited task 2

---
+++ task1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Parameters
+ximax = 8
+ximin = -ximax
+Nsteps = 100*2*ximax
+nu0 = 6.0
+h = (ximax - ximin)/Nsteps
+ep = -nu0/2
+
+
+xi = np.linspace(ximin, ximax, Nsteps)
+
+phi = np.zeros(np.shape(xi))
+
+v = np.array([-nu0*(np.cosh(xi_)**2) for xi_ in xi])
+
+f = v - ep
+
+q = np.sqrt(-ep)
+
+phi[0] = 1
+phi[1] = np.exp(q * h)
+
+for i in range(1, np.size(xi) - 1):
+    phi[i + 1] = (2 + f[i]*h**2)*phi[i] - phi[i - 1]
+
+
--- task2.py
+++ task2.py
@@ -6,3 +6,11 @@ ximax = 8
 ximin = -ximax
 Nsteps = 100*2*ximax
 nu0 = 6.0
+
+s = 2
+
+def v(xi):
+    return xi
+
+def v2(xi):
+    return v(xi + s) + v(xi - s)
