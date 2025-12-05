1c1b3a9bf8c6ac98a135881c12d9ed390ec754a6
BomberGranater <elis.eles@hotmail.com>
2025-12-05 14:47:09 +0100

    blibliblolb

---
+++ task 1.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameters
+ximax=8
+ximin=-ximax
+Nsteps=100*2*ximax
+nu0=6.0
+ep = -nu0/2
+h = (ximax - ximin)/Nsteps
+q = np.sqrt(-ep)
+
+xi = np.linspace(ximin, ximax, Nsteps)
+
+phi = np.zeros(np.size(xi))
+
+nu = np.array([-nu0*(np.cosh(xi_)**(-2)) for xi_ in xi])
+
+f = nu - ep
+
+phi[0] = 1
+phi[1] = np.exp(q*h)
+
+
+for i in range(1, np.size(xi) - 1):
+    phi[i + 1] = (2 + h**2 * f[i]) * phi[i] - phi[i - 1]
+
+plt.plot(xi, phi)
+plt.show()
+
+
+
+
+
