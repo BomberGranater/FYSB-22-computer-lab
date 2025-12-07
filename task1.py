33789edbc6f7ac81c867a5a4366b2f3598b9df20
Lowe Berg <loweberg04@gmail.com>
2025-12-07 11:56:59 +0100

    bisection fixed to align with binary search. Binary search also generalized

--- task1.py
+++ task1.py
@@ -9,7 +9,8 @@ Nsteps = 100*2*ximax
 b = 3
 nu0 = b*(b+1)
 h = (ximax - ximin)/Nsteps
-ep = -4.000
+target = 2  # target number of nodes in search
+ep = -(b-target)**2
 
 
 xi = np.linspace(ximin, ximax, Nsteps)
@@ -52,11 +53,8 @@ def count_nodes(fun):
 
 
 # actual bisection method
-#plt.plot(xi, gen_phi(-6))
-print(f"minimum energy function has {count_nodes(gen_phi(-6))} nodes")
-#plt.figure()
+print(f"minimum energy function has {count_nodes(gen_phi(-nu0))} nodes")
 print(f"maximum energy function has {count_nodes(gen_phi(-0.1))} nodes")
-#plt.plot(xi, gen_phi(-0.1))
 
 
 def find_ep(goal):
@@ -69,11 +67,12 @@ def find_ep(goal):
     nodes_max = count_nodes(phi_max)
 
     if nodes_min > goal or nodes_max < goal:
+        print("Not enough nodes")
         return 404
 
     # find right range for epsilon
     for j in range(500):
-        if abs((ep_min - ep_max)/(ep_min + ep_max)) < 10**-8:
+        if abs(ep_min - ep_max) < 10**-16:
             print("converged")
             break
 
@@ -90,30 +89,72 @@ def find_ep(goal):
 
         test = phi_mid[-2]*phi_mid[-1] - np.exp(np.sqrt(-ep_mid)*h)*(phi_mid[-1]**2)
 
-        if test < 0:
-            tail = phi_mid[-1]
-            if tail * (-1)**goal < 0:
-                ep_max = ep_mid
-                continue
-            else:
-                ep_max = ep_mid
-                continue
+        if test < 1:  # Always look for maximum epsilon
+            ep_min = ep_mid
 
-        if test > 0:  # Bad guess on interval
+        if test > 1:  # Bad guess on interval
+            print("Goal outside search area")
             return 404
 
-    return ep_min, ep_max
+    return (ep_min + ep_max)/2
 
 
-interval = find_ep(0)
-print("right range for one node is", interval)
+# Binary search double check
+left = ep - 0.005
+right = ep + 0.005
+mid = ep
 
-phi = gen_phi(interval[0])
+for i in range(1000):
+    mid = (left + right) / 2
 
-print(f"solution has {count_nodes(phi)} nodes")
+    phi = gen_phi(mid)
 
+    last = phi[-1]
+
+    if last * (-1)**target < 0:
+        right = mid
+
+    if last * (-1)**target > 0:
+        left = mid
+
+
+phi2 = gen_phi(mid)
+
+
+Y = np.zeros(Nsteps)
+
+nodes = np.zeros(Nsteps)
+
+eps = np.linspace(-nu0, -0.1, Nsteps)
+
+for i in range(Nsteps):
+    phi = gen_phi(eps[i])
+    Y[i] = phi[-2]*phi[-1] - np.exp(np.sqrt(-eps[i])*h)*(phi[-1]**2)
+    nodes[i] = count_nodes(phi)
+
+
+ep = find_ep(target)
+phi = gen_phi(ep)
+
+
+# normalize
+phi = phi/np.max(phi)
+
+phi2 = phi2/np.max(phi2)
+
+print("middle value bisect:", ep)
+print("binary search value:", mid)
+
+
+plt.plot(xi, phi2)
+plt.title("Binary search")
 plt.figure()
-plt.plot(xi, phi, label='found function')
+plt.plot(xi, phi)
+plt.title("Bisection method")
+plt.figure()
+plt.plot(eps, nodes)
+plt.scatter(ep, count_nodes(phi), label="bisection")
+plt.scatter(mid, count_nodes(phi2), marker="x", label="binary search")
 
 plt.legend()
 plt.show()
